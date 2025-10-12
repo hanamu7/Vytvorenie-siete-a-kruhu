8fffcdeaf8919e2941b2785e7ad6ea16b9bda98a
davidm-bio <davidmarekgrey@gmail.com>
2025-10-12 22:07:18 +0200

    Add files via upload

---
+++ Semestrálka PAI1.cpp
@@ -0,0 +1,31 @@
+/* Zadání
+Kartézká síť s pravidelnými buňkami
+Vkládáme křivočarou geometrii
+V buňce sítě: 
+    - najít hranici geometrie 
+    - poměr obsazenosti
+    - normála k hranici 
+
+Doporučení: použijte VTK knihovnu
+*/ 
+
+#include <iostream> 
+
+int main() {
+    // Zformátování vstupu do .vtp souboru
+        // Buď z .stl (3D) nebo vlastní polygon za pomocí vtkSmartPointer<vtkPoints>
+
+        // Získat z dat hrany geometrie <vtkFeatureEdges>
+
+        // Uložit do PolyData/PolyLine
+
+    // Vytvoření sítě 
+    
+    // Umístění geometrie do sítě 
+        // Najít hraniční buňky 
+        
+        // Zjistit obsazenost buňky
+
+        // Normála za pomocí vtkPolyDataNormals 
+}
+
